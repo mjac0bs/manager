9e667e2c60c7143505db7f3b2914f79720815f36
mjac0bs <mjacobs@akamai.com>
2024-01-19 13:48:07 -0700

    Attempt to test useHistory instead of location reload

--- packages/manager/src/features/TopMenu/UserMenu/UserMenu.tsx
+++ packages/manager/src/features/TopMenu/UserMenu/UserMenu.tsx
@@ -22,6 +22,7 @@ import { useGrants } from 'src/queries/profile';
 import { getStorage, setStorage, storage } from 'src/utilities/storage';
 
 import { SwitchAccountDrawer } from './SwitchAccountDrawer';
+import { useHistory } from 'react-router-dom';
 
 interface MenuLink {
   display: string;
@@ -56,6 +57,7 @@ export const UserMenu = React.memo(() => {
   } = useAccountManagement();
 
   const flags = useFlags();
+  const history = useHistory();
 
   const currentToken = useCurrentToken();
 
@@ -123,6 +125,12 @@ export const UserMenu = React.memo(() => {
     setAnchorEl(null);
   };
 
+  // Navigate to the current location, triggering a re-render without a full page reload.
+  const refreshPage = React.useCallback(() => {
+    // TODO: Parent/Child: We need to test this against the real API.
+    history.push(history.location.pathname);
+  }, [history]);
+
   /**
    * Clear account switching token errors when menu closes.
    */
@@ -177,9 +185,12 @@ export const UserMenu = React.memo(() => {
     setStorage('authentication/expire', activeExpiry);
 
     // Using a timeout just to witness the token switch in the dev console.
-    setTimeout(() => {
-      location.reload();
-    }, 2000);
+    // setTimeout(() => {
+    //   location.reload();
+    // }, 2000);
+    // location.reload();
+
+    refreshPage();
   };
 
   const open = Boolean(anchorEl);
